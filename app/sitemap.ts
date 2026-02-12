6d71600a67196ddd3a69f3524a9ee8760d020387
Sahu21 <55915491+Sahu21@users.noreply.github.com>
2026-02-12 13:58:28 +0530

    Update base URL in sitemap function

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next'
 import { BLOG_POSTS } from '@/constants'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://sfai.netlify.app'
+  const baseUrl = 'https://genuine-clafoutis-f792ee.netlify.app'
   
   const routes = [
     '',
